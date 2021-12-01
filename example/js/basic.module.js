0db2131a1282b31b019fafe89bb00fb140ac29d5
bernie <mail@javaner.de>
2021-12-01 13:58:22 +0100

    add: example

--- example/js/basic.module.js
+++ example/js/basic.module.js
@@ -1,74 +1,84 @@
-import * as THREE from "../../node_modules/three/build/three.module.js";
-import addVolumetricSpotlightMaterial2DatGui from "../../dist/datgui.volumetricSpotlight.module.js"
-import Volumetricspotlight from "../../dist/Volumetricspotlight.module.js"
-import Viewport from "../../node_modules/three-viewport/dist/viewport.es.js"
+import * as THREE from "./vendor/three.module.js";
+import addVolumetricSpotlightMaterial2DatGui from "../../src/datgui.volumetricSpotlight.module.js"
+import Volumetricspotlight from "./volumetricspotlight.module.js"
+import Viewport from "./vendor/viewport.es.js"
 import stage1 from "./stage1.module.js"
 
 
-	let VP = new Viewport();
-
-    VP.init();
-	VP.start();
 	
-	//document.body.appendChild( VP.renderer.domElement );
-	VP.renderer.setClearColor('black', 0 );
-	VP.renderer.shadowMap.enabled	= true;
+let VP = new Viewport();
 
-	VP.camera.position.set(0,2,5);
-	VP.camera.lookAt(VP.scene.position);
+VP.init();
+VP.start();
 
-    // add a ambient light
-    VP.scene.add( new THREE.AmbientLight( 0x666666 ) );
+//document.body.appendChild( VP.renderer.domElement );
+VP.renderer.setClearColor('black', 0 );
+VP.renderer.shadowMap.enabled	= true;
 
-	VP.scene.fog	= new THREE.FogExp2( 0x000000, 0.1 );
+VP.camera.position.set(0,2,5);
+VP.camera.lookAt(VP.scene.position);
 
-	
-	//////////////////////////////////////////////////////////////////////////////////
-	//		create a scene							//
-	//////////////////////////////////////////////////////////////////////////////////
+// add a ambient light
+VP.scene.add( new THREE.AmbientLight( 0x666666 ) );
 
-	stage1( VP );
+VP.scene.fog	= new THREE.FogExp2( 0x000000, 0.1 );
 
 
-	//////////////////////////////////////////////////////////////////////////////////
-	//		animate the volumetric spotLight				//
-	//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+//		create a scene							//
+//////////////////////////////////////////////////////////////////////////////////
 
-	VP.loop.add(function( delta, now ){
-		var angle	= 0.1 * Math.PI*2*now;
-		target.set( 1*Math.cos(angle), 0, 1*Math.sin(angle) );
-		spotLight.update();
-	});
+stage1( VP );
 
 
-	//////////////////////////////////////////////////////////////////////////////////
-	//		add a volumetric spotligth					//
-	//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+//		animate the volumetric spotLight				//
+//////////////////////////////////////////////////////////////////////////////////
 
-	let angle =.1;
-	const target	= new THREE.Vector3(1*Math.cos(angle),0,1*Math.sin(angle));
-	var spotLight	= new Volumetricspotlight({follow: target });
-	
-	spotLight.position.set( 1.5,2,0 );
-	
-	VP.scene.add( spotLight );
-	VP.scene.add( spotLight.light.target );
-	
+VP.loop.add(function( delta, now ){
+	var angle	= 0.1 * Math.PI*2*now;
+	target.set( 1*Math.cos(angle), 0, 1*Math.sin(angle) );
+	spotLight.update();
+	spotLight2.update();
+	spotLight3.update();
+});
 
-	//////////////////////////////////////////////////////////////////////////////////
-	//		link it with a spotLight					//
-	//////////////////////////////////////////////////////////////////////////////////
 
-	let lightHelper = new THREE.SpotLightHelper( spotLight.light );
-	let shadowCameraHelper = new THREE.CameraHelper( spotLight.light.shadow.camera );
+//////////////////////////////////////////////////////////////////////////////////
+//		add a volumetric spotligth					//
+//////////////////////////////////////////////////////////////////////////////////
 
-	VP.scene.add( lightHelper );
-	VP.scene.add( shadowCameraHelper );
+let angle =.1;
+const target	= new THREE.Vector3(1*Math.cos(angle),0,1*Math.sin(angle));
+var spotLight	= new Volumetricspotlight({color:"green", target: target });
 
-	VP.loop.add(function( delta, now ){
-		lightHelper.update();
-		shadowCameraHelper.update();
-	});
-	
-	// add a DAT.Gui for fine tuning
-	new addVolumetricSpotlightMaterial2DatGui( spotLight );
+spotLight.position.set( 1.5, 2, 0 );
+
+
+var spotLight2	= new Volumetricspotlight({color:"red", target: target });
+spotLight2.position.set( -1.5, 2, 0 );
+
+var spotLight3	= new Volumetricspotlight({color:"blue", target: target });
+spotLight3.position.set( 0, 2, 1.5 );
+
+VP.scene.add( spotLight, spotLight2, spotLight3 );
+
+
+VP.scene.add( spotLight.light.target );
+//////////////////////////////////////////////////////////////////////////////////
+//		link it with a spotLight					//
+//////////////////////////////////////////////////////////////////////////////////
+
+let lightHelper = new THREE.SpotLightHelper( spotLight.light );
+let shadowCameraHelper = new THREE.CameraHelper( spotLight.light.shadow.camera );
+
+VP.scene.add( lightHelper );
+VP.scene.add( shadowCameraHelper );
+
+VP.loop.add(function( delta, now ){
+	lightHelper.update();
+	shadowCameraHelper.update();
+});
+
+// add a DAT.Gui for fine tuning
+new addVolumetricSpotlightMaterial2DatGui( spotLight );
